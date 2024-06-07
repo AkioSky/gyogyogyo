aaa6251cad0a026104592770203c0f4e1a45fbd6
nktmrk <nktmrk@outlook.com>
2024-06-07 07:42:10 -0700

    remove unnecessary code in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import Dashboard from './dashboard/page';
-import { cookies } from 'next/headers';
 import Login from './login/page';
 
-export async function getSession() {
-  const sessionId = cookies().get('sessionId')?.value;
-  // return sessionId ? await db.findSession(sessionId) : null;
-}
-
 export default async function Home() {
-  const session = await getSession();
-
   return <Login />;
 }
